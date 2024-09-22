172d159d4f689d28083510e65f2c84154efa988e
Noureldin <138480588+Noureldin5@users.noreply.github.com>
2024-09-22 20:05:24 +0600

    Idk anymore

--- Integral/integral_or_smth.f90
+++ Integral/integral_or_smth.f90
@@ -9,10 +9,10 @@ b = 3*(4*datan(1.0d0))/2
 S = 0.0
 do while(i<M)
 r = (b-a)*rand() + a
-S = sin(r) + S
+S = (sin(r)*r)+ S
 print*,S/i
 i = i + 1
 end do
-result =(S/M)*r
+result =(S/M)
 print*,"Result:",result
 end program integral_or_smth
